d7565fa9398151646f3beb14191818be13b00f49
CarlosIGF <carlos.igformacion@gmail.com>
2020-11-10 09:38:49 +0100

    comimt

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,8 +7,6 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!11111");
-            Console.WriteLine("Hello World!22222");
-            Console.WriteLine("Hello World!33333333333");
         }
     }
 }
