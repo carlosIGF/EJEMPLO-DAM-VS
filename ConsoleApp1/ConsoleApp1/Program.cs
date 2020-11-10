38d0945bad73153f5b7ccee207f8dc560342a916
carlosIGF <70751105+carlosIGF@users.noreply.github.com>
2020-11-10 18:48:03 +0100

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,12 +7,6 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!11111");
-            Console.WriteLine("Hello World!22222");
-            Console.WriteLine("Hello World!33333333333");
-            Console.WriteLine("Hello World!444");
-            Console.WriteLine("Hello World!5");
-            Console.WriteLine("Hello World!5");
         }
     }
 }
